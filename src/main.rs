0fb83caf1bf3b7baba1e7905a996dbbe648e0960
ETsBrother <etsbrother1@gmail.com>
2022-06-02 08:33:28 -0400

    variables, if/for, and arrays

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
     println!("Hello world");
+
+    let mut x: i32 = 12;
+    let mut y: f32 = 2.0;
+    x += 5;
+    y *= 1.5;
+    println!("x/y = {}", x / y as i32);
+
+    if x == 4 {
+        println!("x is 4");
+    } else if x <= 3 {
+        println!("x is less than 4");
+    } else {
+        println!("x is greater than 4")
+    }
+
+    let mut a: [i32; 4] = [1, 2, 3, 4];
+    a[2] = 4;
+    println!("len of a is {}", a.len());
+    for elem in a {
+        print!("{} ", elem);
+    }
+    println!();
+    
+    for i in 1..101 {
+        if i % 3 == 0 && i % 5 == 0 {
+            print!("FizzBuzz ");
+        } else if i % 3 == 0 {
+            print!("Fizz ");
+        } else if i % 5 == 0 {
+            print!("Buzz ");
+        } else {
+            print!("{} ", i);
+        }
+    }
+    println!();
 }
